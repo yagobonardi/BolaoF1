b6df6b7d41edd7d1154cba35d01062b5613bb013
yagobonardi <yagobonardi@gmail.com>
2024-12-13 20:32:18 -0300

    ideia atualizacao pontuacao para varios usuarios de uma vez

--- update_points.sql
+++ update_points.sql
@@ -1,2 +1,2 @@
 --Exemplo de como deve ser a atualizacao dos pontos para varios usuarios de uma vez
-UPDATE tbl_user SET points = 4 WHERE Id in (1,2,3,4,5)
+UPDATE tbl_user SET points = points + 4 WHERE Id in (1,2,3,4,5)
